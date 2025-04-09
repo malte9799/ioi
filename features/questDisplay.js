cf6fb2d3e5b50c604eef7c3ed83ab9fc4650014c
[MΛLTE] <maltem.hilpert@gmx.de>
2025-04-09 12:28:04 +0200

    fix: everything

--- features/questDisplay.js
+++ features/questDisplay.js
@@ -2,7 +2,6 @@
 /// <reference lib="es2015" />
 import Feature from 'ioi/class/Feature';
 import PogObject from 'PogData';
-import db from 'ioi/db';
 import huds from 'ioi/huds';
 
 const Background = com.chattriggers.ctjs.api.render.Display.Background;
@@ -24,58 +23,58 @@ class QuestDisplay extends Feature {
 
 		this.isDefaultEnabled = true;
 
-		// this.DB = new PogObject('ioi', { active: {} }, 'data/quests.data.json');
-		// this.DB.autosave();
-		if (!db.quests) db.quests = [];
+		this.DB = new PogObject('ioi', { quests: {} }, 'data/quests.data.json');
+		this.DB.autosave();
 
 		this.questTracker = {};
 
-		// this.display = new Display({ x: 4, y: Renderer.screen.getHeight() / 2, background: Background.FULL }).addLine('§6§l    Active Quests    ');
+		this.display = new Display({ x: 4, y: Renderer.screen.getHeight() / 2, background: Background.FULL }).addLine('§6§l    Active Quests    ');
 	}
 
 	initSettings(Settings) {}
 
 	onEnable() {
-		this.registerEvent('renderOverlay', () => {
-			if (huds.isOpen()) return;
-
-			Renderer.pushMatrix().translate(textHud.getX(), textHud.getY()).scale(textHud.getScale());
-			Renderer.drawRect(Renderer.getColor(0, 0, 0, 80), -2, -2, textHud.getWidth() + 4, textHud.getHeight() + 4);
-			Renderer.drawStringWithShadow(textHud.text, 0, 0);
-			Renderer.popMatrix();
-		});
-		this.registerStep(false, 1, () => {
-			// textHud.text =
-			// Object.entries(this.DB.active)
-			// 	.map(([questName, quest]) => {
-			// 		const percent = Math.round((quest.progress / quest.goal) * 10000) / 100;
-			// 		return [percent, `§6§l| §r${questName}: \n§6§l|   §a${quest.progress}§7/§c${quest.goal} §7[${percent}%]`];
-			// 	})
-			// 	.sort((a, b) => b[0] - a[0])
-			// 	.forEach(([, line], i) => {
-			// 		this.display.setLine(i + 1, line);
-			// 	});
-		});
-
-		// this.loadQuestTriggers();
-
 		// this.registerEvent('renderOverlay', () => {
-		// 	Object.entries(this.DB.active)
-		// 		.map(([questName, quest]) => {
-		// 			const percent = Math.round((quest.progress / quest.goal) * 10000) / 100;
-		// 			return [percent, `§6§l| §r${questName}: \n§6§l|   §a${quest.progress}§7/§c${quest.goal} §7[${percent}%]`];
-		// 		})
-		// 		.sort((a, b) => b[0] - a[0])
-		// 		.forEach(([, line], i) => {
-		// 			this.display.setLine(i + 1, line);
-		// 		});
+		// 	if (huds.isOpen()) return;
 
-		// 	this.display.setY((Renderer.screen.getHeight() - this.display.getHeight()) / 2).draw();
+		// 	Renderer.pushMatrix().translate(textHud.getX(), textHud.getY()).scale(textHud.getScale());
+		// 	Renderer.drawRect(Renderer.getColor(0, 0, 0, 80), -2, -2, textHud.getWidth() + 4, textHud.getHeight() + 4);
+		// 	Renderer.drawStringWithShadow(textHud.text, 0, 0);
+		// 	Renderer.popMatrix();
 		// });
+		// this.registerStep(false, 1, () => {
+		// 	// textHud.text =
+		// 	// Object.entries(this.DB.quests)
+		// 	// 	.map(([questName, quest]) => {
+		// 	// 		const percent = Math.round((quest.progress / quest.goal) * 10000) / 100;
+		// 	// 		return [percent, `§6§l| §r${questName}: \n§6§l|   §a${quest.progress}§7/§c${quest.goal} §7[${percent}%]`];
+		// 	// 	})
+		// 	// 	.sort((a, b) => b[0] - a[0])
+		// 	// 	.forEach(([, line], i) => {
+		// 	// 		this.display.setLine(i + 1, line);
+		// 	// 	});
+		// });
+
+		this.loadQuestTriggers();
+
+		this.registerEvent('renderOverlay', () => {
+			Object.entries(this.DB.quests)
+				.map(([questName, quest]) => {
+					const percent = Math.round((quest.progress / quest.goal) * 10000) / 100;
+					return [percent, `§6§l| §r${questName}: \n§6§l|   §a${quest.progress}§7/§c${quest.goal} §7[${percent}%]`];
+				})
+				.sort((a, b) => b[0] - a[0])
+				.slice(0, 5)
+				.forEach(([, line], i) => {
+					this.display.setLine(i + 1, line);
+				});
+
+			this.display.setY((Renderer.screen.getHeight() - this.display.getHeight()) / 2).draw();
+		});
 
 		this.registerEvent('guiOpened', (gui, event) => {
 			if (gui.getTitle().getString() !== 'Active Quests') return;
-			// this.DB.active = {};
+			// this.DB.quests = {};
 			let quests = [];
 			Client.scheduleTask(1, () => {
 				const items = Player.getContainer().getItems().slice(28, 44);
@@ -95,22 +94,25 @@ class QuestDisplay extends Feature {
 						.split(' - ')
 						.slice(1);
 
-					quests.push({
-						name,
-						goal: parseInt(goal),
-						progress: parseInt(progress),
-						item: item.getType().getRegistryName(),
-						objectives,
-					});
-					// this.DB.active[name] = {
+					// quests.push({
+					// 	name,
 					// 	goal: parseInt(goal),
 					// 	progress: parseInt(progress),
-					// 	objectives,
 					// 	item: item.getType().getRegistryName(),
-					// };
+					// 	objectives,
+					// });
+					this.DB.quests[name] = {
+						goal: parseInt(goal),
+						progress: parseInt(progress),
+						objectives,
+						item: item.getType().getRegistryName(),
+					};
 				});
+				// quests.forEach((quest) => {
+				// 	this.DB.quests[quest.name] = quest;
+				// });
 				// console.dir(quests);
-				console.log(JSON.stringify(quests, undefined, 2));
+				// console.log(JSON.stringify(quests, undefined, 2));
 				// this.loadQuestTriggers();
 			});
 		});
@@ -118,14 +120,14 @@ class QuestDisplay extends Feature {
 		this.registerChat('| QUESTS | You completed ${questName}!', (questName) => {
 			this.questTracker[questName].unregister();
 			delete this.questTracker[questName];
-			// delete this.DB.active[questName];
+			delete this.DB.quests[questName];
 			this.display.clearLines();
 		});
 
 		this.registerChat('| RANKUP | You have ranked up to ${rank}', () => {
 			Object.values(this.questTracker).forEach((e) => e.unregister());
 			this.questTracker = {};
-			// this.DB.active = {};
+			this.DB.quests = {};
 			this.display.clearLines();
 		});
 	}
@@ -158,16 +160,16 @@ class QuestDisplay extends Feature {
 		Object.values(this.questTracker).forEach((e) => e.unregister());
 		this.questTracker = {};
 
-		Object.entries(this.DB.active).forEach(([questName, quest]) => {
+		Object.entries(this.DB.quests).forEach(([questName, quest]) => {
 			if (quest.progress >= quest.goal) {
-				delete this.DB.active[questName];
+				delete this.DB.quests[questName];
 				return;
 			}
 			quest.objectives.forEach((o, i) => {
 				if (/Collect [\d,]* Logs/.test(o)) {
 					const event = this.registerEvent('blockBreak', (block) => {
 						if (!/minecraft:\w*_log/.test(block.type.getRegistryName())) return;
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
@@ -179,7 +181,7 @@ class QuestDisplay extends Feature {
 						.replace('coal', 'coal_ore');
 					const event = this.registerEvent('blockBreak', (block) => {
 						if (filterBlock !== 'blocks' && !new RegExp(`minecraft:${filterBlock}`).test(block)) return;
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
@@ -194,14 +196,14 @@ class QuestDisplay extends Feature {
 							.anyMatch((e) => e.getKey().getValues().getLast() == e.getValue());
 						if (!fullyGrown) return;
 
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 
 				if (/Catch [\d,]* Fish/.test(o)) {
 					this.registerChat('| FISH | You caught a ${size}cm Fish', () => {
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 				}
 				if (/Catch [\d,]* \w*\+ Fish/.test(o)) {
@@ -209,55 +211,52 @@ class QuestDisplay extends Feature {
 						const rarity = o.match(/Catch [\d,]* (\w*)\+ Fish/)[1].toLowerCase();
 						const rarityList = fishRarity.slice(fishRarity.indexOf(rarity));
 
-						if (rarityList.includes(rarity.toLowerCase())) this.DB.active[questName].progress++;
+						if (rarityList.includes(rarity.toLowerCase())) this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 				if (/Find [\d,]* Mine Crates/.test(o)) {
 					const event = this.registerChat('| CRATES | You just received a crate. Claim it with /claim!', () => {
-						const tool = Player.getHeldItem()?.getType()?.getRegistryName();
-						if (!tool) if (!Player.getHeldItem()?.getType()?.getRegistryName()?.includes('_pickaxe')) return;
-						this.DB.active[questName].progress++;
+						if (!Player.getHeldItem()?.getType()?.getRegistryName()?.includes('_pickaxe')) return;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 				if (/Find [\d,]* Fishing Crates/.test(o)) {
 					const event = this.registerChat('| CRATES | You just received a crate. Claim it with /claim!', () => {
-						const tool = Player.getHeldItem()?.getType()?.getRegistryName();
-						if (!tool) if (!Player.getHeldItem()?.getType()?.getRegistryName() == 'minecraft:fishing_rod') return;
-						this.DB.active[questName].progress++;
+						if (!Player.getHeldItem()?.getType()?.getRegistryName() == 'minecraft:fishing_rod') return;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 				if (/Find [\d,]* Farming Crates/.test(o)) {
 					const event = this.registerChat('| CRATES | You just received a crate. Claim it with /claim!', () => {
-						const tool = Player.getHeldItem()?.getType()?.getRegistryName();
-						if (!tool) if (!Player.getHeldItem()?.getType()?.getRegistryName()?.includes('_hoe')) return;
-						this.DB.active[questName].progress++;
+						if (!Player.getHeldItem()?.getType()?.getRegistryName()?.includes('_hoe')) return;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 				if (/Use the Superbreaker Mining Ability [\d]* times/.test(o)) {
 					const event = this.registerChat('**SUPER BREAKER ACTIVATED**', () => {
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 				if (/Vote for the Server [\d,]* times/.test(o)) {
 					const event = this.registerChat(`| VOTE | ${Player.getName()} voted on \${server} +1 Voting Shard!`, () => {
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 				if (/Earn [\d,]* McMMO Levels/.test(o)) {
 					const event = this.registerChat('| MCMMO | ${skill} increased to ${level}.', (skill, level) => {
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
 				if (/Loot [\d]* Chambers/.test(o)) {
 					const event = this.registerChat('| CHAMBERS | You just looted a ${chamber} Chamber!', (chamber) => {
-						this.DB.active[questName].progress++;
+						this.DB.quests[questName].progress++;
 					});
 					this.questTracker[questName] = event;
 				}
