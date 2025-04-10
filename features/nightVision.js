93338e8763e409e2917d63ebdf74320cdf372e83
Malte9799 <maltem.hilpert@gmx.de>
2025-04-10 13:07:18 +0200

    feat: night Vision

---
+++ features/nightVision.js
@@ -0,0 +1,32 @@
+/// <reference types="../../CTAutocomplete" />
+/// <reference lib="es2015" />
+import Feature from 'ioi/class/Feature';
+
+const StatusEffectInstance = net.minecraft.entity.effect.StatusEffectInstance;
+const StatusEffects = net.minecraft.entity.effect.StatusEffects;
+
+class NightVision extends Feature {
+	constructor() {
+		super();
+
+		this.isDefaultEnabled = true;
+	}
+
+	initSettings(Settings) {}
+
+	onEnable() {
+		setTimeout(() => {
+			Player.toMC().addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, -1, 255, false, false));
+		}, 100);
+		this.registerEvent('worldLoad', () => {
+			setTimeout(() => {
+				Player.toMC().addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, -1, 255, false, false));
+			}, 100);
+		});
+	}
+
+	onDisable() {}
+}
+module.exports = {
+	class: new NightVision(),
+};
