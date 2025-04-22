db9e985bd25c2e4d13242f0a0affd18c3097c090
[MΛLTE] <maltem.hilpert@gmx.de>
2025-04-22 10:45:14 +0200

    chore: cleanup

--- features/mining/chambers.js
+++ features/mining/chambers.js
@@ -15,24 +15,11 @@ class Chambers extends Feature {
 	initSettings(Settings) {}
 
 	onEnable() {
-		// this.registerChat('| CHAMBERS | You have entered a secret chamber!', () => {});
 		this.registerChat('| CHAMBERS | You sense a great chamber near by...', () => {
 			Client.showTitle('', '&7You sense a great chamber nearby...', 5, 40, 10);
 		});
 		this.registerChat('| CHAMBERS | You sense a chamber near by...', () => {
 			Client.showTitle('', '&7You sense a chamber nearby...', 5, 40, 10);
-		});
-		this.registerSoundPlay('minecraft:entity.elder_guardian.ambient', (pos) => {
-			// const s = new Sound({ source: 'entity.warden.heartbeat', x: pos.x, y: pos.y, z: pos.z });
-			// new Thread(() => {
-			// 	s.play();
-			// 	Thread.sleep(100);
-			// 	s.play();
-			// 	Thread.sleep(100);
-			// 	s.play();
-			// 	Thread.sleep(100);
-			// 	s.play();
-			// }).start();
 		});
 	}
 
