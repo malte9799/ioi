933624efc10b96e6b1fb2499ecfe1490b2ad402e
[MΛLTE] <maltem.hilpert@gmx.de>
2025-04-22 10:46:08 +0200

    refactor: added mapRange()

--- utils/util.js
+++ utils/util.js
@@ -6,21 +6,42 @@ const NbtOps = net.minecraft.nbt.NbtOps;
 const AbstractNbtList = net.minecraft.nbt.AbstractNbtList;
 const NbtCompound = net.minecraft.nbt.NbtCompound;
 
-export default Utils = {
-	itemToJson(item) {},
-	nbtToJson(nbt) {
-		nbt = ComponentMap.CODEC.encodeStart(Player.toMC().getRegistryManager().getOps(NbtOps.INSTANCE), nbt).getOrThrow();
-	},
-};
+export default class Utils {
+	/**
+	 * Maps a number from one range to another.
+	 * Optionally clamps the output to the output range.
+	 *
+	 * @param {number} value The number to map.
+	 * @param {number} inMin The minimum value of the input range.
+	 * @param {number} inMax The maximum value of the input range.
+	 * @param {number} outMin The minimum value of the output range.
+	 * @param {number} outMax The maximum value of the output range.
+	 * @param {boolean} [clamp=false] Whether to clamp the output value to the output range.
+	 * @returns {number} The mapped value.
+	 */
+	static mapRange(value, inMin, inMax, outMin, outMax, clamp = false) {
+		const normalizedValue = (value - inMin) / (inMax - inMin);
+		let mappedValue = outMin + normalizedValue * (outMax - outMin);
 
-const unwrapTag = () => {};
-
-const addCompound = (nbt) => {
-	nbt.getKeys().forEach((key) => {
-		const nested = nbt.get(key) instanceof AbstractNbtList || nbt.get(key) instanceof NbtCompound;
-		if (nested) {
+		if (clamp) {
+			// Determine the actual min and max of the output range
+			const actualOutMin = Math.min(outMin, outMax);
+			const actualOutMax = Math.max(outMin, outMax);
+			mappedValue = Math.max(actualOutMin, Math.min(mappedValue, actualOutMax));
 		}
-	});
-};
-const addList = () => {};
-const addValue = () => {};
+
+		return mappedValue;
+	}
+}
+
+// const unwrapTag = () => {};
+
+// const addCompound = (nbt) => {
+// 	nbt.getKeys().forEach((key) => {
+// 		const nested = nbt.get(key) instanceof AbstractNbtList || nbt.get(key) instanceof NbtCompound;
+// 		if (nested) {
+// 		}
+// 	});
+// };
+// const addList = () => {};
+// const addValue = () => {};
